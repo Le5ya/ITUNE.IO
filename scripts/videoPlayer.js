beb63446fdcaeae6aa8a8821e049f81271fd9ce3
Leya Leya <liliatolstyh@yandex.ru>
2020-07-30 16:22:45 +0300

    musickPlayer realisation

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,4 @@
+import { addZero } from './supportScript.js';
 export const videoPlayerInit =() => {
 const videoPlayer=document.querySelector('.video-player');
 const videoButtonPlay=document.querySelector('.video-button__play');
@@ -32,7 +33,7 @@ const stopPlay = () => {
   videoPlayer.pause();
   videoPlayer.currentTime = 0;
 };
-const addZero = n => n < 10 ? '0'+ n : n;
+//const addZero = n => n < 10 ? '0'+ n : n;
 
 videoPlayer.addEventListener('click', togglePlay );
 videoButtonPlay.addEventListener('click', togglePlay );
